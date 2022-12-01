62f15b584f805bb41a7b7443a541ee838cbba743
Jesper Munkeby <munkeby.j@gmail.com>
2022-12-01 12:23:12 +0100

    resolved merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import './App.css';
-<<<<<<< HEAD
 import BeatDetailPresenter from './components/presenters/BeatDetailPresenter';
 import ModelContext from './contexts/ModelContext';
 import Model from './model';
-=======
-import MainButton, { MainButtonType } from './button';
->>>>>>> be24aeb30819b8fc5bdf9ed589dd890f42931f56
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <ModelContext.Provider value={new Model()}>
 
         <BeatDetailPresenter/>
 
       </ModelContext.Provider>
-=======
-      <MainButton type={MainButtonType.ChooseColorTheme} text = "pick color theme"></MainButton>
-      <MainButton type={MainButtonType.Edit} text = "edit"></MainButton>
-      <MainButton type={MainButtonType.Create} text = "add sample"></MainButton>
->>>>>>> be24aeb30819b8fc5bdf9ed589dd890f42931f56
     </div>
   );
 }
