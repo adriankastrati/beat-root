ae626785f312f00f1daf7655828dc1eed890072b
Adrian Kastrati <77054963+adriankastrati@users.noreply.github.com>
2022-12-12 17:37:55 +0100

    refactored feed, fetch blobs Co-authored-by kastrati@kth.se | https://github.com/adriankastrati Co-authored-by werik@kth.se | https://github.com/Hallmane

---
+++ src/components/views/common/feedViewElements.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const BeatParent = styled.div`
+width:400px;
+height:400px;
+border-radius: 5%;
+position:relative;
+display: flex;
+background-color: rgb(236, 228, 178);
+justify-content: center;
+align-content: center;
+margin: 5%;
+`;
+export const ThemedCard = styled.div`
+width: 400px;
+height: 350px;
+border-radius: 5%;
+position: relative;
+display: flex;
+//background-color: --;
+background-color: rgb(176, 115, 255);
+justify-content: center;
+align-content: center;
+margin: 5%;
+`;
+export const ButtonsContainer = styled.div`
+position: absolute; 
+top: 90%;
+right: 78%;
+`;
+export const CardButton = styled.button`
+`;
+
