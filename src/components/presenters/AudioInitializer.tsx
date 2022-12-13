c56c72695ee9d9789036d2357320acb3f3408030
Deni Persson <deni.persson@gmail.com>
2022-12-13 19:18:22 +0100

    weird button fix

--- src/components/presenters/AudioInitializer.tsx
+++ src/components/presenters/AudioInitializer.tsx
@@ -1,14 +1,18 @@
 import { Children, ReactNode, useContext, useState } from "react";
 import ModelContext from "../../contexts/ModelContext";
+import styled from "styled-components";
 
-
+const MarginDiv = styled.div`
+    margin: 40px;
+`
 
 export default function AudioInitializer(props:{children: ReactNode | ReactNode[]}){
 
     const {audioModel} = useContext(ModelContext)
     const [initialized, setInitialized] = useState(audioModel.initialized);
 
-    return initialized ? 
-        <div>{props.children}</div> :
-        <button onClick={()=>audioModel.init().then(()=>setInitialized(true))}>initialize</button>
+    return (<MarginDiv> {initialized ? 
+                <div>{props.children}</div> :
+                <button onClick={()=>audioModel.init().then(()=>setInitialized(true))}>initialize</button>}
+            </MarginDiv>)
 }
