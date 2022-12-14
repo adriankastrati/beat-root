c9928c499bec93647bb04c1364ce8b764783f26e
Adrian Kastrati <77054963+adriankastrati@users.noreply.github.com>
2022-12-14 12:51:20 +0100

    new function to set username and profile picture

--- src/components/presenters/UserPagePresenter.tsx
+++ src/components/presenters/UserPagePresenter.tsx
@@ -1,9 +1,11 @@
 import UserPageView from "../views/UserPageView";
-import { getCurrentUserID, getUserInformation, isUserLoggedIn, UserInformation } from "../../model/firebase/firebaseAuthenticationModel";
+import { getCurrentUserID, getUserInformation, isUserLoggedIn, setUsername, UserInformation } from "../../model/firebase/firebaseAuthenticationModel";
 import { useEffect, useState } from "react";
 
 export default function UserPagePresenter(){
     const[userInformation, setUserInformation] = useState<UserInformation>()
+    const[profileChange, setProfileChange] = useState<string>()
+
     useEffect(()=>{
         isUserLoggedIn().then(log =>{
             if(log){
@@ -21,10 +23,28 @@ export default function UserPagePresenter(){
         }
     , [])
 
+    async function changeUsername(username: string){
+        return setUsername(username).then(acheived=>{
+            if (acheived)
+                setProfileChange("Completed")
+            else
+                setProfileChange("Failed")
+        })
+    }
+
+    async function changePicture(pictureLink: string){
+        return setUsername(pictureLink).then(acheived=>{
+            if (acheived)
+                setProfileChange("Completed")
+            else
+                setProfileChange("Failed")
+        })
+    }
 
     return<div>
        {
         <UserPageView
+            onUpdateUserName = {changeUsername}
             username = {userInformation?userInformation.username:null}
             email={userInformation?userInformation.email:null}
             id={userInformation?userInformation.authID:null}
