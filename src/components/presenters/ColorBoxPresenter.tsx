a41067a7f4d4a3f9303e6c0b30bbc7c605dbe8ba
Hallmane <ericssonwilliam96@gmail.com>
2022-12-15 12:07:32 +0100

    colorbox works if the fetched colors create a box large enough for the ref to not be seen during first render.

--- src/components/presenters/ColorBoxPresenter.tsx
+++ src/components/presenters/ColorBoxPresenter.tsx
@@ -1,69 +1,59 @@
 import {useState, useEffect, useRef, useInsertionEffect} from "react"
-import { RouteComponentProps } from "react-router-dom"
 import { useIntersection } from "react-use"
 import { getColorRandomScheme } from "../../common/colorSource"
 import ColorBoxView from "../views/ColorBoxView"
-import ColorSchemeBox from "../views/common/ColorSchemeBox"
 
 
-// small problem: new fetches might hold already fetched colors
-// medium problem: I need to render multiple ColorBoxes
-// larger problems: re-fetch on which threshold? All items are visible atm
+// fix 
 const ColorBoxPresenter = () => {
-    const [colorArray, setColorArray] = useState<string[]>([]) // single bar of colors
     const [themeArray, setThemeArray] = useState<string[][]>([])       // array of bars
-    //const [offset, setOffset] = useState(0)
-    const [loading, setLoading] = useState(false)
 
-    const targetRef = useRef<HTMLDivElement | null>(null)
+    const [shouldFetch, setShouldFetch] = useState(false)
+
+    const [rerender, setRerender] = useState(new Object())
+
+    const targetRef = useRef<HTMLDivElement | null>(null) // null might be the root of issues
     const intersection = useIntersection(targetRef, {
         root:null,
         rootMargin: '200px',
         threshold: 1.0,
     })
-
-    const AMOUNT_COLORS = 4
-    const AMOUNT_FETCHES = 1
-
-    function fetchThemes() {
-        setLoading(true)
-
-        getColorRandomScheme(AMOUNT_COLORS) // returns [#, #, #, #]
-        .then((theme) => {if(theme){
-            themeArray.push(theme)
-            setThemeArray(themeArray) 
-            console.log("setting themes:", themeArray)
-        }else {
-            console.log("response: ", theme)
-        }})
-
-
-        console.log(themeArray)
-        //setThemeArray(themeArray.concat(colorArray))
-        setLoading(false)
-    }
+    const AMOUNT_FETCHES = 8
 
     useEffect(() => {
-        if (intersection?.isIntersecting && !loading) {
-            console.log("intersecting!")
-            //setOffset(offset + AMOUNT_FETCHES)
-            fetchThemes()
-            if ( colorArray.keys.length == 0) {
-                console.log("there are no themes, fetching...")
-                fetchThemes()
-            } else {
-                console.log("there are themes")
-                return
-            }
+        if (intersection?.isIntersecting && !shouldFetch) {
+            setShouldFetch(true)
         }
 
-    },[intersection?.isIntersecting])
+    },[intersection])
+
+    useEffect(()=>{
+        if (shouldFetch) {
+            console.log(Math.floor(Math.random()*6))
+            //fetch one time
+            Promise.all( new Array(AMOUNT_FETCHES).fill(0).map(()=>getColorRandomScheme(Math.floor(Math.random()*5+1))))
+            .then(newStuff => {
+                setThemeArray([
+                    ...themeArray,
+                    ...newStuff
+                ])})
+        } else{
+            setRerender(new Object)
+        }
+        setShouldFetch(false)
+        }
+
+        , [shouldFetch])
 
     return (
+        //<div>
+        //    {themeArray.map((theme,index)=><div key={index}>{theme[0]}</div>)}
+        //    <div ref={targetRef}>baj</div>
+        //</div>
         <ColorBoxView 
-            //themeArray = { themeArray }
-            //targetRef = { targetRef }
-            //loading = { loading }
+            themeArray = { themeArray }
+            targetRef = { targetRef }
+            loading = { false}//TODO
         />
     )
 }
