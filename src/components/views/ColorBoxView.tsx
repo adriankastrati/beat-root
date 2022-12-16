14328c251f5171b2b1afd94fda38e0174ca2d343
Deni Persson <denip@kth.se>
2022-12-16 13:50:22 +0100

    fixed styling on color box a bit

--- src/components/views/ColorBoxView.tsx
+++ src/components/views/ColorBoxView.tsx
@@ -11,29 +11,29 @@ const TitleStyle = styled.div`
     margin:0px;
 `
 const OuterBox = styled.div`
-  display:box;
-  height: 400px;
-  flex: none;
+  display:flex;
+  height: 450px;
   overflow-y: scroll;
   flex-direction: column;
   margin:40px;
+  width: 60%;
+  align-items:center;
 `
 const InnerBox = styled.div`
+  display: flex;
   flex-direction:column;
-  padding: 10px;
-  border: 1px solid ${theme.medium};
-  width: 300px;
-`
-const Center = styled.div`
   align-items: center;
-  align-self: center;
-  margin: 0 auto;
+  padding: 10px;
+  
+  border: 1px solid ${theme.medium};
+  width: 80%;
 `
 const BoxWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   align-items: center;
-  align-self: center;
-
-`;
+  
+`
 
 
 interface ColorBoxViewProps {
@@ -48,26 +48,25 @@ interface ColorBoxViewProps {
 function ColorBoxView(props: ColorBoxViewProps) {
 
   return (
-    <BoxWrapper>
-          <TitleStyle>Color Theme</TitleStyle>
-      <OuterBox>
-          {props.themeArray && 
-          !props.loading? props.themeArray.map((theme, key) => {
-              return (
-                <InnerBox key={key} onClick={()=>{props.onSetChosen(key)}}
-                  style={{backgroundColor:props.chosen === key ? "gray":"white"}}
-                >
-                  <ColorSchemeBox key={key} colorArray={theme}></ColorSchemeBox>
-                </InnerBox> 
-              )
-            }) : <p>loading...</p> }
-          <div ref={props.targetRef}>ref for scroll fetch</div>
-      </OuterBox>
-      <Center>
-        <MainButton type={MainButtonType.Plain} text="continue" scale={1} width={100} 
-        onClick={props.onContinue}/>
-      </Center>
-    </BoxWrapper>
+            <BoxWrapper>
+                  <TitleStyle>Color Theme</TitleStyle>
+              <OuterBox>
+                  {props.themeArray && 
+                  !props.loading? props.themeArray.map((theme, key) => {
+                      return (
+                        <InnerBox key={key} onClick={()=>{props.onSetChosen(key)}}
+                          style={{backgroundColor:props.chosen === key ? "gray":"white"}}
+                        >
+                          <ColorSchemeBox key={key} colorArray={theme}></ColorSchemeBox>
+                        </InnerBox> 
+                      )
+                    }) : <p>loading...</p> }
+                  <div ref={props.targetRef}>ref for scroll fetch</div>
+              </OuterBox>
+              
+                <MainButton type={MainButtonType.Plain} text="continue" scale={1} width={100} 
+                onClick={props.onContinue}/>
+            </BoxWrapper>
     )
   }
 
