248ceec7bf144f7c9eef441851bba8cd4fa9d0ea
Hallmane <ericssonwilliam96@gmail.com>
2022-12-16 21:24:55 +0100

    added about page

---
+++ src/components/presenters/HomeViewPresenter.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+const HomeViewPresenter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    
+
+    return (
+        <div>
+            uh 
+        </div>
+    )
+}
+
+export default HomeViewPresenter
