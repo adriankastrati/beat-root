cf7e85c663524284b618b2878d952db75f54e146
Jesper Munkeby <munkeby.j@gmail.com>
2022-12-16 19:22:08 +0100

    refactored audiomodel to work in a more robust way. Squashed a bunch of bugs

--- src/model/AudioModel.tsx
+++ src/model/AudioModel.tsx
@@ -1,5 +1,5 @@
 
-import { cloneDeep } from "lodash";
+import { cloneDeep, reject } from "lodash";
 import * as Tone from "tone";
 import { Sampler } from "tone";
 import {Rhythm, Sample, Track} from "../common";
@@ -9,11 +9,16 @@ export default class AudioModel {
     initialized: boolean = false
     samplers: Tone.Sampler[] = []
     samples = new Map<string,string>()
-    playing = false
+    nextID = 0
+    playingID:number|null = null
 
-    play(tracks:Track[], bpm:number){ //TODO: Sample lookup table in initialized model. Sample is only a string
-        this.clear()
-        //create all samplers
+    playTracks(tracks:Track[], bpm:number):Promise<number>{
+
+        this.stop()
+        const id = this.nextID
+        this.playingID = id
+        this.nextID += 1
+
         this.samplers = tracks.map(
             ({sample, rhythm}) => {
                 let loopTime = 60*rhythm.steps/bpm
@@ -25,32 +30,33 @@ export default class AudioModel {
                     baseUrl: this.samples.get(sample),
 
                     onload:()=>{
+
                         //schedule
-                        console.log("play")
                         Tone.Transport.scheduleRepeat((time:number)=>{
-                            console.log(rhythm.getNormalizedLoopSchedule())
                             rhythm.getNormalizedLoopSchedule().forEach(hitTime => {
-                                sampler.triggerAttackRelease("A1", "2", time + hitTime*loopTime);
+                                sampler.triggerAttackRelease("A1", "3", time + hitTime*loopTime);
                                     })
                         }, loopTime)
+
                     }
 
                 }).toDestination()
                 return sampler
             }
         )
-        
-        //start transport
-        Tone.Transport.start(Tone.now())
 
-        this.playing = true
+        return Tone.loaded().then(()=>{
+            Tone.Transport.start()
+            return id
+        })
+
     }
 
     getSamples(){
         return Array.from(this.samples.keys())
     }
 
-    clear(){
+    private clear(){
         //clear transport schedule
         Tone.Transport.cancel(0)
         //dispose all samplers
@@ -61,13 +67,19 @@ export default class AudioModel {
         this.samplers = []
     }
     
-    stop(){
+    stop(id?:number){
+        if (id !== undefined){
+            if (id !== this.playingID){
+                return
+            }
+        }
+
         //clear
         this.clear()
         //stop transport
         Tone.Transport.stop()
 
-        this.playing = false
+        this.playingID = null
     }
 
     init(){
@@ -80,20 +92,3 @@ export default class AudioModel {
         })
     }
 }
-
-
-
-// function mockGetSamples():Promise<{name:string, url:string}[]>{
-//     return new Promise(resolve => setTimeout(resolve, 1000)).then(()=>{
-//         return [
-//             {
-//                 name:"hihat.mp3",
-//                 url:"https://tonejs.github.io/audio/drum-samples/Techno/hihat.mp3" //will be local urls
-//             },
-//             {
-//                 name:"kick.mp3",
-//                 url:"https://tonejs.github.io/audio/drum-samples/Techno/kick.mp3" //will be local urls
-//             }
-//         ] 
-//     })
-// }
--- src/pages/BeatPage.tsx
+++ src/pages/BeatPage.tsx
@@ -1,35 +1,36 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Beat, Rhythm, Sample } from "../common";
-import BeatDetailPresenter from "../components/presenters/BeatDetailPresenter";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { Beat, Rhythm, Sample } from "../common";
+// import BeatDetailPresenter from "../components/presenters/BeatDetailPresenter";
 
-export default function BeatPage(){
-    const params = useParams() as any
-    const [beat, setBeat] = useState<Beat|null>(null)
+// export default function BeatPage(){
+//     const params = useParams() as any
+//     const [beat, setBeat] = useState<Beat|null>(null)
 
-    useEffect(()=>{
-        getDummyBeat(params.beatID).then(beat=>{
-            console.log(beat)
-            setBeat(beat)}
-        )
-    },[])
+//     useEffect(()=>{
+//         getDummyBeat(params.beatID).then(beat=>{
+//             console.log(beat)
+//             setBeat(beat)}
+//         )
+//     },[])
 
-    return beat ? <BeatDetailPresenter beat={beat}/> : <div>TODO: loading animation</div>
-}
+//     return beat ? <BeatDetailPresenter beat={beat}/> : <div>TODO: loading animation</div>
+// }
 
-async function getDummyBeat(beatID:string):Promise<Beat>{
-    return new Promise(resolve => setTimeout(resolve, 1000)).then(()=>{ 
-        return {
-            composerID: "abc123",
-            title: "test beat 123",
-            description: "this is a test beat \nthis is som text after linebreak",
-            theme: ["#2B3A55", "#CE7777", "#E8C4C4"],
-            tracks:[
-                {rhythm:new Rhythm(16), sample: "techno-hihat"},
-                {rhythm:new Rhythm(8), sample: "techno-kick"},
-            ],
-            likes: 15,
-            bpm: 120
-        } as Beat
-    })
-}
+// async function getDummyBeat(beatID:string):Promise<Beat>{
+//     return new Promise(resolve => setTimeout(resolve, 1000)).then(()=>{ 
+//         return {
+//             composerID: "abc123",
+//             title: "test beat 123",
+//             description: "this is a test beat \nthis is som text after linebreak",
+//             theme: ["#2B3A55", "#CE7777", "#E8C4C4"],
+//             tracks:[
+//                 {rhythm:new Rhythm(16), sample: "techno-hihat"},
+//                 {rhythm:new Rhythm(8), sample: "techno-kick"},
+//             ],
+//             likes: 15,
+//             bpm: 120
+//         } as Beat
+//     })
+// }¨
+export {}
