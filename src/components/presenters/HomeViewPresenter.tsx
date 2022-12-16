06ff47ade94ac3389cc1d85b274160565c842717
Hallmane <ericssonwilliam96@gmail.com>
2022-12-16 21:25:37 +0100

    vscode sucks, why do i have to write two commits man

--- src/components/presenters/HomeViewPresenter.tsx
+++ src/components/presenters/HomeViewPresenter.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 
+// implement at a later stage a nice landing page with highlighted beats etc
 const HomeViewPresenter = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
     
