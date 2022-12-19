67d2cc9792804e8e420ebb5be5a84b33657432ac
Deni Persson <deni.persson@gmail.com>
2022-12-19 15:49:34 +0100

    Update UserPagePresenter.tsx

--- src/components/presenters/UserPagePresenter.tsx
+++ src/components/presenters/UserPagePresenter.tsx
@@ -81,7 +81,7 @@ export default function UserPagePresenter(){
         setTimeout(( )=>{ ///it should instead wait for a promise, for whatever info to have finished updated
             fetchUser();
             refresh(new Object)},
-        100)
+        300)
         
     }
 
